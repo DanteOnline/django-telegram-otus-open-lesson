4d851ff69bc7d7a6ca4cb9f955136d98bf0b0e65
DanteOnline <iamdanteonline@gmail.com>
2026-02-12 14:30:41 +0300

    update readme and remove create handler

--- README.md
+++ README.md
@@ -1,5 +1,24 @@
 # django-telegram-otus-open-lesson
 Code for otus open lesson "Django+Telegram bot"
 
-## Examples 
-- [Step 0. Django](https://github.com/DanteOnline/django-telegram-otus-open-lesson/tree/step0-django)
+## Step 3. Sync Djagno + Async Telebot
+
+Ключевая идея в использовании функции `sync_to_async` при работе с Django ORM 
+
+## Usage
+
+```commandline
+pip install -r requirements.txt
+```
+
+```commandline
+make migrate
+```
+
+```commandline
+make server
+```
+
+```commandline
+make run_bot
+```
--- bot/handlers.py
+++ bot/handlers.py
@@ -9,9 +9,9 @@ async def messages_handler(message):
         await bot.send_message(message.chat.id, f'{item_message.text} {item_message.chat_id}')
 
 
-@bot.message_handler(func=lambda message: True)
-async def create_message(message):
-    text = message.text
-    chat_id = message.from_user.id
-    await create_message_in_db(text, chat_id)
-    await bot.send_message(message.chat.id, 'Сообщение сохранено')
+# @bot.message_handler(func=lambda message: True)
+# async def create_message(message):
+#     text = message.text
+#     chat_id = message.from_user.id
+#     await create_message_in_db(text, chat_id)
+#     await bot.send_message(message.chat.id, 'Сообщение сохранено')
